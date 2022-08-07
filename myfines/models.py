164f111cc4545573dadcbcdb372fb87cd53c1627
KrisRomanyuk <tina.mironyuk.96@mail.ru>
2022-08-07 23:08:11 +0300

    models and foreignkey

--- myfines/models.py
+++ myfines/models.py
@@ -8,6 +8,7 @@ class Fines(models.Model):
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     is_published = models.BooleanField(default=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
@@ -15,4 +16,13 @@ class Fines(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_id': self.pk})
 
